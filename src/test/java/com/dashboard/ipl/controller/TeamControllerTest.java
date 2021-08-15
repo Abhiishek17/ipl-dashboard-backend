ad112e6cece077801846c3c978c65da69a7c675c
Abhiishek17 <abhiishek17@gmail.com>
2021-08-16 02:07:48 +0530

    Testing

--- src/test/java/com/dashboard/ipl/controller/TeamControllerTest.java
+++ src/test/java/com/dashboard/ipl/controller/TeamControllerTest.java
@@ -1,5 +1,13 @@
 package com.dashboard.ipl.controller;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class TeamControllerTest {
 
+	@Test
+	public void testFlow() {
+		assertTrue(1 == 1);
+	}
 }
